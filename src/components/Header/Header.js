089e89feaf7cbc90d2007a4097f37cb2c12e0fed
charged ahead <roshan-ican>
2021-08-15 22:40:16 +0530

    will complete this shit tonight

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,9 @@
-import Link from "next/link";
-import React from "react";
-import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from "react-icons/ai";
-import { DiCssdeck } from "react-icons/di";
-
+import {
+  AiFillGithub,
+  AiFillInstagram,
+  AiFillLinkedin,
+  AiFillTwitterCircle,
+} from "react-icons/ai";
 import {
   Container,
   Div1,
@@ -12,13 +13,24 @@ import {
   SocialIcons,
 } from "./HeaderStyles";
 
+import { DiCssdeck } from "react-icons/di";
+import Link from "next/link";
+import React from "react";
+
 const Header = () => (
   <div>
     <Div1>
       <Link href="/">
-        <a style={{ display: "flex", alignItems: "center", color: "white" }}>
+        <a
+          style={{
+            display: "flex",
+            alignItems: "center",
+            color: "white",
+            marginBottom: "20",
+          }}
+        >
           <DiCssdeck size="3rem" />
-          <span>Ibrahim & Muhammad Tech</span>
+          <Span>Portfolio</Span>
         </a>
       </Link>
     </Div1>
@@ -26,7 +38,24 @@ const Header = () => (
       <li href="#projects">
         <NavLink>Project</NavLink>
       </li>
+      <li href="#tech">
+        <NavLink>Technologies</NavLink>
+      </li>
+      <li href="#about">
+        <NavLink>About</NavLink>
+      </li>
     </Div2>
+    <Div3>
+      <SocialIcons href="https://www.github.com/roshan-ican">
+        <AiFillGithub size="3rem" />
+      </SocialIcons>
+      <SocialIcons href="https://www.linkdlen.com">
+        <AiFillLinkedin size="3rem" />
+      </SocialIcons>
+      <SocialIcons href="https://twitter.com/IamMuha51906610">
+        <AiFillTwitterCircle size="3rem" />
+      </SocialIcons>
+    </Div3>
   </div>
 );
 
--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,13 +1,33 @@
-import React from 'react';
+import {
+  Section,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
 
-import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import Button from '../../styles/GlobalComponents/Button';
-import { LeftSection } from './HeroStyles';
+import Button from "../../styles/GlobalComponents/Button";
+import { LeftSection } from "./HeroStyles";
+import React from "react";
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome To <br />
+        IM Tech's Portfolio
+      </SectionTitle>
+      <SectionText>
+        The purpose of 'IM Tech' is to bring our ideas to reality with hardwork
+        and dedication & solve the problems of this World...
+      </SectionText>
+      <Button
+        onClick={() =>
+          (window.location = "https://twitter.com/IamMuha51906610")
+        }
+      >
+        Learn More about us
+      </Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
