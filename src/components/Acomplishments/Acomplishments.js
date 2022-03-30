681012a502726c41f6505b9516531cdea53835bb
Muhammad Roshan ali <66337567+roshan-ican@users.noreply.github.com>
2022-03-30 15:01:28 +0530

    solved alhamduillah

--- src/components/Acomplishments/Acomplishments.js
+++ src/components/Acomplishments/Acomplishments.js
@@ -5,26 +5,38 @@ import {
   SectionDivider,
   SectionTitle,
 } from "../../styles/GlobalComponents";
-import { Box, Boxes, BoxNum, BoxText } from "./AcomplishmentsStyles";
+import { Box, Boxes, BoxNum, BoxText } from "./AcomplishmentsStyles.js";
 
 const data = [
-  { number: 50, text: "Projects with serveal Languages" },
-  { number: 2, text: "full stack react native apps on Playstore" },
-  { number: 1900, text: "Working with 3 clients" },
+  {
+    number: 50,
+    text: "Designed and Developed 50+ frontend & Backend Applications utilizing React.js, Node.js and Python",
+  },
+  {
+    number: 2,
+    text: "Worked with Agile team across nations and have Full stack react native app on Playstore",
+  },
+  {
+    number: 3,
+    text: "Along with code, I have experience with Digital Marketing and currently working with 3 clients",
+  },
+  {
+    number: 100,
+    text: "Have More than 100 Graphic Designing projects, Knows How to do Videography, Photography and Motion Graphics",
+  },
 ];
 
 const Acomplishments = () => (
   <Section>
-    <SectionTitle>Personal Acomplishments</SectionTitle>
+    <SectionTitle> Personal Achievements </SectionTitle>
     <Boxes>
-      {data.map((card, index) => {
+      {data.map((card, index) => (
         <Box key={index}>
-          <BoxNum>{`${card.number}`}+</BoxNum>
+          <BoxNum>{card.number}</BoxNum>
           <BoxText>{card.text}</BoxText>
-        </Box>;
-      })}
+        </Box>
+      ))}
     </Boxes>
-    <SectionDivider />
   </Section>
 );
 
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,10 @@
 import React from "react";
-import { AiFillGithub, AiFillInstagram, AiFillLinkedin, AiFillTwitterCircle } from "react-icons/ai";
+import {
+  AiFillGithub,
+  AiFillInstagram,
+  AiFillLinkedin,
+  AiFillTwitterCircle,
+} from "react-icons/ai";
 
 import { SocialIcons } from "../Header/HeaderStyles";
 import {
@@ -21,31 +26,31 @@ const Footer = () => {
         <LinkColumn>
           <LinkTitle>Call</LinkTitle>
           <LinkItem href="tel:+91-6297-668-173">+91-6297-668-173</LinkItem>
+          <LinkItem href="tel:+91-9733-338-497">+91-9733-388-497</LinkItem>
         </LinkColumn>
         <LinkColumn>
           <LinkTitle>Email</LinkTitle>
-          <LinkItem 
-            href="mailto:roshansahani535@gmail.com">
-              roshansahani535@gmail.com
-            </LinkItem>
+          <LinkItem href="mailto:muhammadroshan788@gmail.com">
+            muhammadroshan788@gmail.com
+          </LinkItem>
         </LinkColumn>
       </LinkList>
-      //contants
       <SocialIconsContainer>
         <CompanyContainer>
-          <Slogan>
-            Turning Ideas into Reality
-          </Slogan>
+          <Slogan>Turning Ideas into Reality</Slogan>
         </CompanyContainer>
         <SocialContainer>
           <SocialIcons href="https://github.com/roshan-ican">
-            <AiFillGithub size="3rem"/>
+            <AiFillGithub size="3rem" />
           </SocialIcons>
           <SocialIcons href="https://www.linkedin.com/in/muhammad-roshan/">
-            <AiFillLinkedin size="3rem"/>
+            <AiFillLinkedin size="3rem" />
           </SocialIcons>
           <SocialIcons href="https://twitter.com/IamMuha51906610">
             <AiFillTwitterCircle size="3rem" />
+          </SocialIcons>
+          <SocialIcons href="https://www.instagram.com/muhammad_roshan_ali">
+            <AiFillInstagram size="3rem" />
           </SocialIcons>
         </SocialContainer>
       </SocialIconsContainer>
